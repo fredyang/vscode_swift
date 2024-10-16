f94bddd43d2a7454f51b76b2ef2031804cef4091
Fred Yang <fredyang@semanticsworks.com>
2024-10-16 01:05:37 -0400

    save

--- Package.swift
+++ Package.swift
@@ -4,11 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "vscode_swift",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "vscode_swift"),
-    ]
+  name: "vscode_swift",
+  dependencies: [
+    .package(
+      name: "IBM Security Verify", url: "https://github.com/ibm-security-verify/verify-sdk-ios.git",
+      from: "3.0.10")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "vscode_swift")
+  ]
 )
